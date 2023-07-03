950166748f1342739541f59a2d45d6f4fd047ac2
mpa <mnpanchal93@github.com>
2023-07-03 22:27:04 +0200

    Make the selection of cell none
    the cell doesn't need to have any effect on selection

--- TodoList/Controller/SwipeTableViewController.swift
+++ TodoList/Controller/SwipeTableViewController.swift
@@ -22,6 +22,7 @@ class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegat
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SwipeTableViewCell
+        cell.selectionStyle = .none
         
         cell.delegate = self
         
